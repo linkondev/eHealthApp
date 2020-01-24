f67b57ed567d60c96d6b0bb4966866ef5c7d9317
Linkon <linkonsid@gmail.com>
2020-01-24 21:33:13 +0600

    iOS app first commit

---
+++ MyHealth/MyHealth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyHealth
+//
+//  Created by Linkon Sid on 24/1/20.
+//  Copyright © 2020 Linkon Sid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
