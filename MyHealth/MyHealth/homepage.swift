5ae63f422320562a7ed692148470b4df8256e6f8
Linkon <linkonsid@gmail.com>
2020-01-31 20:01:39 +0600

    home page added

---
+++ MyHealth/MyHealth/homepage.swift
@@ -0,0 +1,15 @@
+//
+//  homepage.swift
+//  MyHealth
+//
+//  Created by Linkon Sid on 31/1/20.
+//  Copyright © 2020 Linkon Sid. All rights reserved.
+//
+
+import UIKit
+
+class homepage: NSObject {
+    func homecommittest(){
+        print("commit for home")
+    }
+}
