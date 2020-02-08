f9235adf1f3ae472403004474d57cad0138fa68c
Linkon Sid <linkon@sheba.xyz>
2020-02-08 18:43:28 +0600

    push before stash

--- MyHealth/MyHealth/AppDelegate.swift
+++ MyHealth/MyHealth/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("stashed example")
+        print("before stash")
         print("for compare tools check")
         return true
     }
