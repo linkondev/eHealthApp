f3637d4b326d4b0572b2b0cbdc2ab4da191b14fe
Linkon Sid <linkon@sheba.xyz>
2020-02-09 23:29:09 +0600

    diff branches...

--- MyHealth/MyHealth/AppDelegate.swift
+++ MyHealth/MyHealth/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("diff branch")
         print("before stash")
         print("for compare tools check")
         return true
